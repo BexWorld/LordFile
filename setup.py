5e326133237770916e7654de7676421cd23773e7
LordBex <lordibex@protonmail.com>
2023-02-28 18:55:10 +0100

    v0.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name='LordFile',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/BexWorld/LordFile.git',
     license='',
     author='lordbex',
